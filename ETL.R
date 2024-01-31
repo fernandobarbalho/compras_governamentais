b7b6be80d28a776480e751457d4efd23da2b46c4
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2024-01-31 16:29:28 -0300

    dados de fornecedores e tratamento de dados de combustíveis

--- ETL.R
+++ ETL.R
@@ -48,8 +48,121 @@ purrr::map_dfr(meses, function(mes){
 })
 
 
-licitacao%>%
+licitacao_v2<-
+  purrr::map_dfr(meses, function(mes){
+
+    print(mes)
+
+    ano<- ifelse(mes=="12",2022,2023)
+
+    tmp = tempfile(fileext = ".zip")
+
+    #download.file(URL_add,mode = "wb", destfile = tmp, extra = "-R", method = "libcurl")
+
+    download.file(url = paste0(url_base,ano,mes), destfile = tmp, mode = "wb")
+
+    file_name <-  paste0(ano,mes,"_LicitaÃ§Ã£o.csv")
+
+    unzip(tmp,exdir= "data" )
+
+
+
+    read_delim( paste0("data/",file_name),
+                delim = ";", escape_double = FALSE, col_types = cols(`Data Resultado Compra` = col_date(format = "%d/%m/%Y"),
+                                                                     `Data Abertura` = col_date(format = "%d/%m/%Y")),
+                locale = locale(date_names = "pt", date_format = "d%m%Y",
+                                decimal_mark = ",", grouping_mark = ".",
+                                encoding = "Latin1"), trim_ws = TRUE) %>%
+
+      janitor::clean_names() %>%
+      select(
+        numero_licitacao,
+        nome_ug,
+        modalidade_compra,
+        objeto,
+        situacao_licitacao,
+        nome_orgao_superior,
+        nome_orgao,
+        uf,
+        municipio,
+        data_resultado_compra,
+        data_abertura,
+        valor_licitacao
+      )
+
+  })
+
+
+
+participantes_vencedores<-
+  purrr::map_dfr(meses, function(mes){
+
+    print(mes)
+
+    ano<- ifelse(mes=="12",2022,2023)
+
+
+    file_name <-  paste0(ano,mes,"_ParticipantesLicitaÃ§Ã£o.csv")
+
+
+
+
+    read_delim(paste0("data/",file_name),
+             delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ",",
+                                                                 grouping_mark = ".", encoding = "latin1"),
+             trim_ws = TRUE) %>%
+
+
+      janitor::clean_names() %>%
+      filter(flag_vencedor=="SIM") %>%
+      select(
+        numero_licitacao,
+        nome_ug,
+        modalidade_compra,
+        nome_orgao,
+        codigo_participante,
+        nome_participante,
+        flag_vencedor
+      )
+
+  })
+
+
+
+202301_ParticipantesLicitação.csv
+202301_ParticipantesLicitaÃ§Ã£o
+
+licitacao_v2%>%
   saveRDS("licitacao.rds")
 
+dca_despesa_orcamentaria_2022<-
+  get_dca(year = c(2022), annex = "I-D",  entity = "1")
 
+dca_despesa_orcamentaria_2022 %>%
+  saveRDS("dca_despesa_orcamentaria_2022.RDS")
+
+fab<-
+licitacao%>%
+  filter(situacao_licitacao == "Encerrado") %>%
+  select(objeto, uf, valor_licitacao )
+
+
+licitacao%>%
+  filter(situacao_licitacao == "Encerrado") %>%
+  select(objeto, uf, valor_licitacao ) %>%
+  write_csv("objetos_licitacao.csv")
+
+objetos_licitacao<-
+licitacao%>%
+  filter(situacao_licitacao == "Encerrado") %>%
+  select(objeto, uf, valor_licitacao )
+
+objetos_licitacao_atualizado <- read_csv("objetos_licitacao_atualizado.csv")
+
+objetos_licitacao$id <- seq(1:NROW(objetos_licitacao))
+
+objetos_licitacao_atualizado$id <- objetos_licitacao$id
+
+
+objetos_licitacao_aquisicao_combustivel <- read_csv("objetos_licitacao_aquisicao_combustivel.csv")
 
--- análise exploratória.R
+++ análise exploratória.R
@@ -41,10 +41,11 @@ dca_despesa_orcamentaria_2016 %>%
 
 59566689843 + 1058248555
 
-dca_despesa_orcamentaria_2022<-
-  get_dca(year = c(2022), annex = "I-D",  entity = "1")
 
 
+
+3.3.90.39.00 -
+123456789011245
 
 dca_despesa_orcamentaria_2022 %>%
   filter(cod_conta %in% c("DO3.3.90.30.00.00",
@@ -57,11 +58,27 @@ dca_despesa_orcamentaria_2022 %>%
                           "DO3.3.90.37.00.00",
                           "DO4.4.90.51.00.00",
                           "DO4.4.90.52.00.00")) %>%
-  select(conta, valor, coluna) %>%
   summarise(valor_total = sum(valor),
             .by = coluna)
 
+dca_despesa_orcamentaria_2022 %>%
+  filter(cod_conta %in% c("DO3.3.90.30.00.00",
+                          "DO3.3.90.32.00.00",
+                          "DO3.3.90.33.00.00",
+                          "DO3.3.90.35.00.00",
+                          "DO3.3.90.36.00.00",
+                          "DO3.3.90.39.00.00",
+                          "DO3.3.90.38.00.00",
+                          "DO3.3.90.37.00.00",
+                          "DO4.4.90.51.00.00",
+                          "DO4.4.90.52.00.00")) %>%
+  mutate(conta = str_sub(conta,16,200)) %>%
+  select(conta, valor, coluna) %>%
+  filter(coluna = "Despesas Liquidadas")
+
 66555876690 + 1402538720
+
+0.66+0.55
 
 licitacao <- readRDS("~/Github/compras_governamentais/licitacao.rds")
 
@@ -69,4 +86,26 @@ licitacao %>%
   filter(situacao_licitacao == "Encerrado") %>%
   summarise(sum(valor_licitacao))
 
+amostra_combustivel<-
+objetos_licitacao_atualizado %>%
+  filter(combustivel_relacionado == TRUE) %>%
+  slice_sample(n=100)
 
+
+objetos_licitacao_aquisicao_combustivel %>%
+  filter(aquisicao_combustivel == TRUE) %>%
+  summarise(mean(valor_licitacao),
+            median(valor_licitacao),
+            sum(valor_licitacao))
+
+
+licitacao %>%
+  filter(situacao_licitacao == "Encerrado") %>%
+  slice_sample(n=1000) %>%
+  summarise(mean(valor_licitacao),
+            median(valor_licitacao),
+            sum(valor_licitacao))
+
+
+licitacao_v2 %>%
+  filter(situacao_licitacao == "Encerrado") %>%
